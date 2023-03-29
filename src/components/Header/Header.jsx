7dcd6067cc15717f8625b5e3a0ed09ee76dab676
moktubat <shadhinreja@gmail.com>
2023-03-30 03:53:44 +0600

    addedd

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+            <a href="">Home</a>
+            <a href="">Shop</a>
+            <a href="">Order</a>
+            <a href="">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
